bdfb4e4938e0e0ce9b5195ad834986837d2a78fd
Nallekari <nallekari18@gmail.com>
2023-09-30 19:18:26 +0300

    fixed lots of weird issues. Praying to god for salvation#666

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -5,7 +5,7 @@ class_name GameManager
 # var a = 2
 # var b = "text"
 onready var player = $Player
-onready var level  : Level = $Level
+onready var level = $Level
 onready var UI := $UI
 
 # Called when the node enters the scene tree for the first time.
