059f99c67027bcef9fecea97368d31362806c293
CodeRaurus <coderaurus@gmail.com>
2023-09-30 19:19:47 +0300

    Add rng to game manager

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -8,8 +8,11 @@ onready var player = $Player
 onready var level  : Level = $Level
 onready var UI := $UI
 
+var rng
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rng = RandomNumberGenerator.new()
 	UI.initialize()
 	pass # Replace with function body.
 
