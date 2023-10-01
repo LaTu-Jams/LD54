9aaf6817d412b1ac5bdc8a3f4b466c7fb056a42d
CodeRaurus <coderaurus@gmail.com>
2023-10-01 23:08:30 +0300

    Stop mineral glitter when depleted

--- scripts/Mineral.gd
+++ scripts/Mineral.gd
@@ -17,13 +17,14 @@ func _ready():
 		yield(get_tree(), "idle_frame")
 	var time_mod = get_tree().current_scene.rng.randi_range(-5,15)
 	
+	$Glimmer.visible = false
 	glimmer_time = $Glimmer/GlimmerTimer.wait_time + time_mod
 	$Glimmer/GlimmerTimer.wait_time = glimmer_time
 	$Glimmer/GlimmerTimer.start()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if HP < 1:
+	if HP < 0:
 		depleted = true
 		$Sprite.frame = 3
 	elif HP/HP_max <= 0.33 and $Sprite.frame != 2:
@@ -35,21 +36,27 @@ func _process(delta):
 
 func mining():
 	HP -= 1 * get_process_delta_time()
-	if HP < 1:
+	if HP < 0:
 		call_deferred("depleted")
 		return true
-	
-		
-	
+	else:
+		return false
+
+
 func depleted():
 	var new_heat = heat_area.instance()
 	new_heat.global_position = global_position
 	get_parent().add_child(new_heat)
 	$CollisionShape2D.disabled = true
 	remove_from_group("mineral")
+	$Glimmer/GlimmerAnimator.play("RESET")
+	$Glimmer/GlimmerAnimator.stop()
+	$Glimmer.visible = false
+	$Glimmer/GlimmerTimer.stop()
 	# queue_free()
 
 
 func _on_glimmer():
-	print("%s glimmering" % self)
-	$Glimmer/GlimmerAnimator.play("glimmer")
+	if not depleted:
+		print("%s glimmering" % self)
+		$Glimmer/GlimmerAnimator.play("glimmer")
