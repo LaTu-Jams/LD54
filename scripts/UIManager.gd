650c14378fba682dae1eee59b8575e47d0ac1d40
Nallekari <nallekari18@gmail.com>
2023-10-01 20:20:10 +0300

    small fixes to index and removed unnecessary particles node

--- scripts/UIManager.gd
+++ scripts/UIManager.gd
@@ -5,19 +5,19 @@ onready var heat_meter := $HeatMeter
 onready var mineral_meter := $MineralMeter
 
 func initialize():
-	heat_meter.max_value = get_parent().get_child(3).get_node("Player").max_temperature
-	heat_meter.value = get_parent().get_child(3).get_node("Player").current_temperature
-	mineral_meter.max_value = get_parent().get_child(3).minerals_in_level
-	mineral_meter.value = get_parent().get_child(3).minerals_gathered
+	heat_meter.max_value = get_parent().get_child(2).get_node("Player").max_temperature
+	heat_meter.value = get_parent().get_child(2).get_node("Player").current_temperature
+	mineral_meter.max_value = get_parent().get_child(2).minerals_in_level
+	mineral_meter.value = get_parent().get_child(2).minerals_gathered
 
 
 func _process(delta):
 	#print((get_parent().get_node("Level")))
-	if get_parent().get_child(3):
+	if get_parent().get_child(2):
 		#print(get_parent().get_child(3))
 		#print(is_instance_valid(get_parent().get_child(3).get_node("Player")))
-		if is_instance_valid(get_parent().get_child(3).get_node("Player")):
-			heat_meter.value = get_parent().get_child(3).get_node("Player").current_temperature
+		if is_instance_valid(get_parent().get_child(2).get_node("Player")):
+			heat_meter.value = get_parent().get_child(2).get_node("Player").current_temperature
 	
 	mineral_meter.value = get_parent().level.minerals_gathered
 	if mineral_meter.value >= get_parent().level.mineral_goal:
