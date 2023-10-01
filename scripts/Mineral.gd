24e9edfc6b2f7a9fc734c517f73ac6fa116a53a1
CodeRaurus <coderaurus@gmail.com>
2023-10-01 15:43:58 +0300

    Add mineral graphics

--- scripts/Mineral.gd
+++ scripts/Mineral.gd
@@ -4,12 +4,15 @@ class_name Mineral
 const heat_area = preload("res://scenes/HeatArea.tscn")
 
 export var HP: float = 6
+var HP_max
 
 var depleted: bool = false
 var glimmer_time
 
 
 func _ready():
+	HP_max = HP
+	
 	while not is_instance_valid(get_tree().current_scene.rng):
 		yield(get_tree(), "idle_frame")
 	var time_mod = get_tree().current_scene.rng.randi_range(-5,15)
@@ -22,6 +25,13 @@ func _ready():
 func _process(delta):
 	if HP < 1:
 		depleted = true
+		$Sprite.frame = 3
+	elif HP/HP_max <= 0.33 and $Sprite.frame != 2:
+		$Sprite.frame = 2
+	elif HP/HP_max > 0.33 and HP/HP_max <= 0.66 and $Sprite.frame != 1:
+		$Sprite.frame = 1
+	elif HP/HP_max > 0.66 and $Sprite.frame != 0:
+		$Sprite.frame = 0
 
 func mining():
 	HP -= 1 * get_process_delta_time()
@@ -29,11 +39,15 @@ func mining():
 		call_deferred("depleted")
 		return true
 	
+		
+	
 func depleted():
 	var new_heat = heat_area.instance()
 	new_heat.global_position = global_position
 	get_parent().add_child(new_heat)
-	queue_free()
+	$CollisionShape2D.disabled = true
+	remove_from_group("mineral")
+	# queue_free()
 
 
 func _on_glimmer():
