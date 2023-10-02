4000c610502b601a1addb85029a5aa881c8d6cfd
CodeRaurus <coderaurus@gmail.com>
2023-10-02 20:56:18 +0300

    Add fade animations to defeat screen and stop radar from scanning when player has exploded

--- scripts/DefeatScreen.gd
+++ scripts/DefeatScreen.gd
@@ -1,20 +1,14 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Button_pressed():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "modulate", Color.transparent, 0.5)
+	tween.tween_property(self, "visible", false, 0.0)
+	$Button.disabled = true
+	yield(tween, "finished")
 	get_tree().current_scene.restart_level()
--- scripts/Radar.gd
+++ scripts/Radar.gd
@@ -6,6 +6,7 @@ var minerals = []
 var closest = Vector2(10000, 10000)
 var target
 var player
+var is_active = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Sprite.visible = false
@@ -29,17 +30,18 @@ func _process(delta):
 	_radar_timer += delta
 	self.rotation = self.global_position.direction_to(target).angle()
 	
-	if _radar_timer >= 3 and !$AnimationPlayer.is_playing() and get_tree().get_nodes_in_group("mineral").size() > 0:
-		target = _find_mineral()
-		_radar_timer = 0
-		if global_position.distance_to(target) > 40:
-			$Sprite.visible = true
-			$Sprite2.visible = true
-			$AnimationPlayer.play("radar")
-			SoundManager.sound("radar", -3)
-			
-	if _radar_timer > 1.5:
-		$AnimationPlayer.stop()
+	if is_active:
+		if _radar_timer >= 3 and !$AnimationPlayer.is_playing() and get_tree().get_nodes_in_group("mineral").size() > 0:
+			target = _find_mineral()
+			_radar_timer = 0
+			if global_position.distance_to(target) > 40:
+				$Sprite.visible = true
+				$Sprite2.visible = true
+				$AnimationPlayer.play("radar")
+				SoundManager.sound("radar", -3)
+				
+		if _radar_timer > 1.5:
+			$AnimationPlayer.stop()
 
 func _find_mineral():
 	closest = Vector2(10000, 10000)
