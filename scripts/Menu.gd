a91f7cbaefdb13b40ed0eab4609e35f244f23ff3
CodeRaurus <coderaurus@gmail.com>
2023-10-02 16:37:36 +0300

    Connect audio managers to menu UI

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -8,8 +8,13 @@ onready var h_slider_2 = $Panel/VBoxContainer/HSlider2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Panel/VBoxContainer/MusicSlider.max_value = MusicManager.MAX_DB
+	$Panel/VBoxContainer/MusicSlider.min_value = MusicManager.MUTE_DB
+	$Panel/VBoxContainer/MusicSlider.value = -20
 
+	$Panel/VBoxContainer/SoundSlider.max_value = SoundManager.MAX_DB
+	$Panel/VBoxContainer/SoundSlider.min_value = SoundManager.MUTE_DB
+	$Panel/VBoxContainer/SoundSlider.value = -15
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -27,3 +32,12 @@ func _on_Button_pressed():
 	if get_tree().current_scene.game_started:
 		get_tree().paused = false
 	visible = false
+
+
+func _on_music_changed(value):
+	MusicManager._on_range_change(value)
+
+
+
+func _on_sound_changed(value):
+	SoundManager._on_range_change(value)
